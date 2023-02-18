b52a8a6f376064855bb7a38de78dfc2daa596fb2
Felipe <41008398+StrawberryMaster@users.noreply.github.com>
2023-02-18 23:29:06 +0000

    Cleanup
    It's not like we're fetching the background image from somewhere else, right?

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,38 @@
 const appPrefix = 'TrackGen';
-const appVersion = 'v1.0.0';
+const appVersion = 'v1.0.1';
 const cacheName = `${appPrefix}-${appVersion}`;
+const filesToCache = [
+    '/',
+    'manifest.json',
+    'index.html',
+    'static/media/favicon.png',
+    'static/media/cyclone.png',
+    'static/media/background.png',
+    'static/media/bg8192.png',
+    'static/js/sw.js',
+    'static/js/rsmc.js',
+    'static/js/new_point.js',
+    'static/js/hurdat.js',
+    'static/js/pages.js',
+    'static/js/ibtracs.js',
+    'static/js/manual_input.js',
+    'static/js/generate.js',
+    'static/js/atcf.js',
+    'static/js/file_upload.js',
+    'static/css/style.css'
+];
 
 self.addEventListener('fetch', e => {
     console.log('Fetch event for ', e.request.url);
     e.respondWith((async () => {
-        const requestUrl = new URL(e.request.url);
-        const isBgImage = requestUrl.pathname === 'TrackGen/static/media/bg8192.png';
-        const cache = await caches.open(cacheName);
-
-        if (isBgImage) {
-            const cachedBgResponse = await cache.match('TrackGen/static/media/bg8192.png');
-            if (cachedBgResponse) {
-                console.log('Returning cached background image...');
-                return cachedBgResponse;
+        cache.match(e.request).then(function(response) {
+            if (response) {
+                console.log('Returning cached response: ', response);
+                return response;
             }
-        }
-
-        const response = await cache.match(e.request) || fetch(e.request);
-
-        console.log('Fetching', e.request.url);
-
-        console.log('Caching', e.request.url);
-
-        await cache.put(e.request, response.clone());
-
-        return response;
+            console.log('Fetching request from the network: ', e.request.url);
+            return fetch(e.request);
+        })
     })());
 });
 
@@ -33,25 +40,7 @@ self.addEventListener('install', e => {
     e.waitUntil(
         caches.open(cacheName).then(cache => {
             console.log("Installing cache: " + cacheName);
-            cache.addAll([
-                "/",
-                "manifest.json",
-                "index.html",
-                "static/media/favicon.png",
-                "static/media/cyclone.png",
-                "static/media/background.png",
-                "static/js/sw.js",
-                "static/js/rsmc.js",
-                "static/js/new_point.js",
-                "static/js/hurdat.js",
-                "static/js/pages.js",
-                "static/js/ibtracs.js",
-                "static/js/manual_input.js",
-                "static/js/generate.js",
-                "static/js/atcf.js",
-                "static/js/file_upload.js",
-                "static/css/style.css"
-            ])
+            cache.addAll(filesToCache)
                 .then(() => { console.log("Cached files!") })
         })
     );
