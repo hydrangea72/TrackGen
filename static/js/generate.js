10f5d0087630a854fcd53e03c0fe778fb7098932
hydrangea72.314 <63913368+hydrangea72@users.noreply.github.com>
2023-08-04 18:26:35 -0400

    Update generate.js

--- static/js/generate.js
+++ static/js/generate.js
@@ -47,8 +47,8 @@ function createMap(data, accessible) {
                 const FULL_WIDTH = BLUE_MARBLE.width;
                 const FULL_HEIGHT = BLUE_MARBLE.height;
 
-                const DOT_SIZE = 0.29890625 / 1200 * FULL_WIDTH;
-                const LINE_SIZE = 0.09 / 900 * FULL_WIDTH;
+                const DOT_SIZE = 0.29890625 / 100 * FULL_WIDTH;
+                const LINE_SIZE = 0.09 / 100 * FULL_WIDTH;
 
                 let max_lat = 0;
                 let max_long = 0;
