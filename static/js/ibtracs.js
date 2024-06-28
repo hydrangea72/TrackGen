bd691743804857f3e35bca4cfc4d0f761f2b4a3d
Felipe <41008398+StrawberryMaster@users.noreply.github.com>
2024-06-28 15:03:33 +0000

    IBTRACS improvements Somehow all of this was broken. This now parses IBTRACS v4 tracks correctly

--- static/js/ibtracs.js
+++ static/js/ibtracs.js
@@ -1,35 +1,57 @@
 function getIbtracsShape(initials) {
-    const i = initials.toUpperCase();
+    const i = initials.trim().toUpperCase();
 
-    if (["TS", "NR", "MX"].includes(i)) {
+    if (["TD", "TS", "HU", "ST", "TY", "NR", "MX"].includes(i)) {
         return "circle";
     } else if (["SS"].includes(i)) {
         return "square";
-    } else if (["ET", "DS"].includes(i)) {
+    } else if (["DB", "ET", "DS"].includes(i)) {
         return "triangle";
     } else {
         console.error("Unknown IBTRACS initials: " + i);
     }
 }
 
+/**
+ * Parses a coordinate value and returns a formatted string.
+ * We insert a decimal point one place from the end of the numeric part,
+ * and append the direction character. For example, "1234E" becomes "123.4E".
+ * 
+ * @param {string} value - The coordinate value to parse.
+ * @returns {string} The formatted coordinate value.
+ */
+function parseCoord(value) {
+    const len = value.length;
+    const direction = value.charAt(len - 1); // Extract the last character (E, W, N, S)
+    const numericValue = value.substring(0, len - 1);
+
+    const formattedValue = numericValue.length > 1
+        ? numericValue.slice(0, -1) + '.' + numericValue.slice(-1)
+        : '0.' + numericValue;
+
+    return parseFloat(formattedValue).toFixed(1) + direction;
+}
+
 function parseIbtracs(data) {
-    const lines = data.split("\n").slice(2);
+    const lines = data.split("\n");
 
     const parsed = [];
     lines.forEach(line => {
         if (line !== "") {
             const cols = line.split(",");
 
-            const wmo_wind = Number(cols[10]);
-            const usa_wind = Number(cols[23]);
+            const wmo_wind = Number(cols[8]);
+            const usa_wind = cols[21] ? Number(cols[21]) : NaN;
+
+            const max_wind = isNaN(usa_wind) ? wmo_wind : Math.max(wmo_wind, usa_wind);
 
             parsed.push(
                 {
                     name: cols[0],
-                    shape: getIbtracsShape(cols[7]),
-                    category: speedToCat(Math.max(wmo_wind, usa_wind)),
-                    latitude: cols[8] + "N",
-                    longitude: cols[9] + "E"
+                    shape: getIbtracsShape(cols[10]),
+                    category: speedToCat(max_wind),
+                    latitude: parseCoord(cols[6].trim()),
+                    longitude: parseCoord(cols[7].trim())
                 }
             );
         }
